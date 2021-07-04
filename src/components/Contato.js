8acee9e906b5e4d0cccc2ef42383d24c8da0125a
Kayo <51717883+jkayoss@users.noreply.github.com>
2021-07-04 18:09:46 -0300

    Add files via upload

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -12,12 +12,12 @@ export function ContatoMe() {
             
                 <h1>Tem algum projeto frontend?</h1>
                 <h2>Fala comigo ↓ </h2>
-                <form action="https://send.pageclip.co/a3tL3emY0rlVgN0YPJas3aybU5fZLayW" class="pageclip-form" name="contato" method="POST">
+                <form action="https://send.pageclip.co/a3tL3emY0rlVgN0YPJas3aybU5fZLayW" onSubmit={alertSending} class="pageclip-form" name="contato" method="POST">
                     <input type="text" name="nome" placeholder="Informe seu nome"/>
                     <input type="email" name="email" placeholder="Informe um e-mail"/>
                     <input type="tel" name="telefone" placeholder="Informe um telefone"/>
                     <textarea name="mensagem" placeholder="Como posso ajuda-lo?"></textarea>
-                    <button type="submit" onClick={alertSending}>Enviar mensagem</button>
+                    <button type="submit">Enviar mensagem</button>
                 </form>
                 <footer class="footer"></footer>
                 <div class="contextMenu">
